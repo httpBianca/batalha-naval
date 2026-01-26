d572692604e187be7c9a62410c52cbb016016723
Bianca Oliveira <biancavelvet13@gmail.com>
2026-01-26 20:09:12 -0300

    navio nas diagonais

--- naval.c
+++ naval.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 
+#define linhas 10
+#define colunas 10
+
 int main () {
     //declaração da matriz tabuleiro matriz (10x10)
-    int tabuleiro [10][10];
+    int tabuleiro [linhas][colunas];
 
     //inicialização da matriz com tudo água (0)
-    for (int i=0; i<10; i++) {
-        for (int j=0; j<10; j++) {
-            tabuleiro[i][j] = 0; //água (0)
+    for (int i=0; i<linhas; i++) {
+        for (int j=0; j<colunas; j++) {//colocando navio na diagonal
+            if(i == j && i>6){ //diagonal principal
+                tabuleiro[i][j] = 3;
+            }
+            else if(i==j && i<3){
+                tabuleiro[i][j] = 3;
+            }
+            else if(i+j == 9 && i<3){ //diagonal secundaria
+                tabuleiro[i][j] = 3;
+            }
+            else{
+                tabuleiro[i][j] = 0; //água (0)
+            }
         }
     }
 
-    //navios (3)
-    tabuleiro[1][2] = 3; //linha 2, coluna C
-    tabuleiro[1][3] = 3;
-    tabuleiro[4][6] = 3; //linha 5, coluna G
-    tabuleiro[4][7] = 3; 
 
     //Colunas de letras A-J
     printf(" ");
-    for(int j = 0; j < 10; j++){
+    for(int j = 0; j < colunas; j++){
         printf(" %c", 'A' + j); // 'A' + 0 = A, 'A' + 1 = B ...
     }
     printf("\n");
 
     //impressão da matriz
-    for (int i=0; i<10; i++) {
+    for (int i=0; i<linhas; i++) {
         printf("%d", i + 1);
-        for (int j=0; j<10; j++) {
+        for (int j=0; j<colunas; j++) {
             printf(" %d", tabuleiro[i][j]);
         }
         printf("\n"); //espaçamento entre colunas e linhas
